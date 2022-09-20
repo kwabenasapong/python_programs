4486dc16dfc1e3985c1ea8b1b1a045651eb734cd
Kwabena Sapong <kwabenasapong@gmail.com>
2022-09-20 13:35:13 +0000

    unit testing files

---
+++ unit_test/circles.py
@@ -0,0 +1,7 @@
+'''#!/usr/bin/python3'''
+from import import pi
+'''function to check the area of a circle
+'''
+def circle_area(r):
+    return pi *(r**2)
+
---
+++ unit_test/test_circles.py
@@ -0,0 +1,11 @@
+import unittest
+from circles import circle_area
+from math import pi
+
+class TestCircleArea(unittest.TestCase):
+    '''To check circleArea'''
+    def test_area(self):
+        '''Test areas when radius >= 0'''
+        self.assertAlmostEqual(circle_area(1), pi)
+        self.assertAlmostEqual(circle_area(0), 0)
+        self.assertAlmostEqual(circle_area(2.1, pi * 2.1**2))
